b1a1c22f55920b29b0c8d12b19e1ec6cdf2a78f5
sema <semanur_ersoy20@erdogan.edu.tr>
2022-03-14 16:01:02 +0300

    Commit Added

---
+++ ce100-hw1-sln/ce100-hw1-algo-lib-cs/Class1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ce100_hw1_algo_lib_cs
+{
+
+
+    public class Class1
+    {
+
+        /**
+        * 
+        * @name InsertionSort
+        * 
+        * @param [in] arr [\b int[]]
+         
+        * Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time.
+        * Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration.
+        **/
+        public static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1],
+                // that are greater than key,
+                // to one position ahead of
+                // their current position
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        /**
+        * 
+        * @name SelectionSort
+        * 
+        * @param [in] arr [\b int[]]
+         
+        * Selection sort is an in-place comparison sorting algorithm.
+        **/
+
+        public static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // One by one move boundary of unsorted subarray
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first
+                // element
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        /**
+        * 
+        * @name Merge
+        * 
+        * @param [in] numbers [\b int[]]
+        * @param [in] left [\b int]
+        * @param [in] mid [\b int]
+        * @param [in] right [\b int]
+       
+        * Merge sort is a recursive algorithm that continually splits a list in half. 
+        **/
+        public static void Merge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        /**
+        * 
+        * @name RecursiveMergeSort
+        * 
+        * @param [in] arr [\b int[]]
+        * retval [\b int []] 
+        * Merge sort is a recursive algorithm that continually splits a list in half. 
+        **/
+        public static void RecursiveMergeSort(int[] numbers, int left, int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                RecursiveMergeSort(numbers, left, mid);
+                RecursiveMergeSort(numbers, (mid + 1), right);
+                Merge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        /**
+        * 
+        * @name RecursivevePower
+        * 
+        * @param [in] arr [\b int[]]
+        * retval [\b long ] 
+        * To find power of any number, you can use pow() function. result = pow
+        **/
+        public static int RecursivePower(int n, int powerRaised)
+        {
+            if (powerRaised != 0)
+                return (n * RecursivePower(n, powerRaised - 1));
+            else
+                return 1;
+        }
+
+        /**
+        * 
+        * @name NaivePower
+        * 
+        * @param [in] arr [\b int[]]
+        * retval [\b int []] 
+        * Returns the result of an exponential number.
+        **/
+        public static long Naive_Power(int a, int p)
+        {
+            long pow = 1L;
+            for (int i = 1; i <= p; i++)
+            {
+                pow = pow * a;
+            }
+            return pow;
+
+        }
+
+        /**
+        * 
+        * @name RecursiveBinarySearch
+        * 
+        * @param [in] arr [\b int[]]
+        * retval [\b int []] 
+        * Recursive binary search is an implementation of the binary search algorithm that uses recursive method calls.
+        **/
+
+        public static int RecursiveBinarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+
+                if (arr[mid] == x)
+                    return mid;
+
+
+                if (arr[mid] > x)
+                    return RecursiveBinarySearch(arr, l, mid - 1, x);
+
+
+                return RecursiveBinarySearch(arr, mid + 1, r, x);
+            }
+            return -1;
+        }
+
+        /**
+        * 
+        * @name IterativeBinarySearch
+        * 
+        * @param [in] arr [\b int[]]
+        * retval [\b int []] 
+        * Binary search begins by comparing the middle element of the array to the target value.
+        * •	If the target value is equal to the middle element, its position in the array is returned.
+        **/
+        public static int IterativeBinarySearch(int[] arr, int x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+
+
+                if (arr[m] == x)
+                    return m;
+
+                if (arr[m] < x)
+                    l = m + 1;
+
+                else
+                    r = m - 1;
+            }
+            return -1;
+        }
+
+        /**
+        * 
+        * @name MasterMethod
+        * 
+        * @param [in] x [\b int]
+        * @param [in] y [\b int]
+        * @param [in] f [\b string]     
+        **/
+        public static string MasterMethod(int x, int y, string f)
+        {
+            double e = Math.Log(Convert.ToDouble(x), Convert.ToDouble(y));
+            double a;
+            try
+            {
+                a = Convert.ToDouble(f.Replace("n^", ""));
+
+            }
+            catch (Exception)
+            {
+                a = 1;
+            }
+
+            if (x >= 1)
+            {
+                if (e > a)
+                {
+                    return "T(n) = n^" + e;
+                }
+                else if (e == a)
+                {
+                    return "T(n) = n^" + e + "logn";
+                }
+                else if (e < a)
+                {
+                    return "T(n) = n^" + a;
+                }
+            }
+            return "Incalculable!";
+        }
+
+
+
+    }
+}
+
+
+
+
---
+++ ce100-hw1-sln/ce100-hw1-algo-test-cs/UnitTest1.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ce100_hw1_algo_test_cs;
+using ce100_hw1_algo_lib_cs;
+
+namespace ce100_hw1_algo_test_cs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+            public void InsertionSort_Test1()
+            {
+                int[] array = { 13, 5, 34, 22, 18 };
+                Class1.InsertionSort(array);
+                int[] result = { 5, 13, 18, 22, 34 };
+                CollectionAssert.AreEqual(array, result);
+
+            }
+
+            [TestMethod]
+            public void InsertionSort_Test2()
+            {
+                int[] array = { 7, 16, 38, 13, 25, 22 };
+                Class1.InsertionSort(array);
+                int[] result = { 7, 13, 16, 22, 25, 38 };
+                CollectionAssert.AreEqual(array, result);
+
+            }
+            [TestMethod]
+            public void InsertionSort_Test3()
+            {
+                int[] array = { 27, 58, 61, 47, 18, 8, 34 };
+                Class1.InsertionSort(array);
+                int[] result = { 8, 18, 27, 34, 47, 58, 61 };
+                CollectionAssert.AreEqual(array, result);
+            }
+            [TestMethod]
+            public void SelectionSort_Test1()
+            {
+                int[] array = { 5, 10, 3, 36, 42, 27 };
+                Class1.SelectionSort(array);
+                int[] result = { 3, 5, 10, 27, 36, 42 };
+                CollectionAssert.AreEqual(array, result);
+
+            }
+            [TestMethod]
+            public void SelectionSort_Test2()
+            {
+                int[] array = { 36, 7, 87, 27, 13, 46, 68 };
+                Class1.SelectionSort(array);
+                int[] result = { 7, 13, 27, 36, 46, 68, 87 };
+                CollectionAssert.AreEqual(array, result);
+            }
+
+            [TestMethod]
+            public void SelectionSort_Test3()
+            {
+                int[] array = { 32, 5, 72, 18, 56, 13, 25 };
+                Class1.SelectionSort(array);
+                int[] result = { 5, 13, 18, 25, 32, 56, 72 };
+                CollectionAssert.AreEqual(array, result);
+            }
+
+            [TestMethod]
+            public void RecursiveMergeSort_Test1()
+            {
+                int[] array = { 25, 4, 38, 12, 58, 42 };
+                Class1.RecursiveMergeSort(array, 0, array.Length - 1);
+                int[] result = { 4, 12, 25, 38, 42, 58 };
+                CollectionAssert.AreEqual(array, result);
+            }
+
+            [TestMethod]
+            public void RecursiveMergeSort_Test2()
+            {
+                int[] array = { 13, 7, 65, 24, 42, 35 };
+                Class1.RecursiveMergeSort(array, 0, array.Length - 1);
+                int[] result = { 7, 13, 24, 35, 42, 65 };
+                CollectionAssert.AreEqual(array, result);
+            }
+
+            [TestMethod]
+            public void RecursiveMergeSort_Test3()
+            {
+                int[] array = { 46, 78, 4, 26, 14, 35 };
+                Class1.RecursiveMergeSort(array, 0, array.Length - 1);
+                int[] result = { 4, 14, 26, 35, 46, 78 };
+                CollectionAssert.AreEqual(array, result);
+            }
+        [TestMethod]
+        public void RecursivePower_Test1()
+        {
+            Assert.AreEqual(Class1.RecursivePower(6, 3), 216);
+
+        }
+        [TestMethod]
+        public void RecursivePower_Test2()
+        {
+            Assert.AreEqual(Class1.RecursivePower(7, 2), 49);
+
+        }
+        [TestMethod]
+        public void RecursivePower_Test3()
+        {
+            Assert.AreEqual(Class1.RecursivePower(569, 0), 1);
+
+        }
+        [TestMethod]
+        public void NaivePower_Test1()
+        {
+            Assert.AreEqual(Class1.Naive_Power(3, 4), 81);
+        }
+
+        [TestMethod]
+        public void NaivePower_Test2()
+        {
+            Assert.AreEqual(Class1.Naive_Power(16, 2), 256);
+        }
+
+        [TestMethod]
+        public void NaivePower_Test3()
+        {
+            Assert.AreEqual(Class1.Naive_Power(8, 3), 512);
+        }
+
+        [TestMethod]
+        public void RecursiveBinarySearch_Test1()
+        {
+            int[] array = { 7, 9, 14, 18, 27, 36, 45 };
+            int n = array.Length;
+            int a = 27;
+            Assert.AreEqual(Class1.RecursiveBinarySearch(array, 0, n - 1, a), 4);
+        }
+
+        [TestMethod]
+        public void RecursiveBinarySearch_Test2()
+        {
+            int[] array = { 8, 10, 17, 38, 105, 156, 189, 210, 252 };
+            int n = array.Length;
+            int a = 38;
+            Assert.AreEqual(Class1.RecursiveBinarySearch(array, 0, n - 1, a), 3);
+        }
+
+        [TestMethod]
+        public void RecursiveBinarySearch_Test3()
+        {
+            int[] array = { 3, 12, 27, 65, 72, 85, 92, 141 };
+            int n = array.Length;
+            int a = 85;
+            Assert.AreEqual(Class1.RecursiveBinarySearch(array, 0, n - 1, a), 5);
+        }
+
+        [TestMethod]
+        public void IterativeBinarySearch_Test1()
+        {
+            int[] array = { 8, 38, 56, 65, 92, 178 };
+            int a = 8;
+            Assert.AreEqual(Class1.IterativeBinarySearch(array, a), 0);
+        }
+
+        [TestMethod]
+        public void IterativeBinarySearch_Test2()
+        {
+            int[] array = { 19, 25, 45, 62, 75, 87, 102, 189, 228 };
+            int a = 189;
+            Assert.AreEqual(Class1.IterativeBinarySearch(array, a), 7);
+        }
+
+        [TestMethod]
+        public void IterativeBinarySearch_Test3()
+        {
+            int[] array = { 32, 56, 75, 92, 125, 224, 241, 266, 351 };
+            int a = 75;
+            Assert.AreEqual(Class1.IterativeBinarySearch(array, a), 2);
+        }
+
+        [TestMethod]
+        public void MasterMethod_Test1()
+        {
+            Assert.AreEqual(Class1.MasterMethod(10, 100, "n^2"), "T(n) = n^2");
+        }
+
+        [TestMethod]
+        public void MasterMethod_Test2()
+        {
+            Assert.AreEqual(Class1.MasterMethod(5, 125, "n^3"), "T(n) = n^3");
+        }
+
+        [TestMethod]
+        public void MasterMethod_Test3()
+        {
+            Assert.AreEqual(Class1.MasterMethod(4, 4, "n^1"), "T(n) = n^1logn");
+        }
+    }
+}
+    
+
